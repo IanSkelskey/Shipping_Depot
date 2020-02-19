c6e005b9d4ee6a4861a363355b455aa45f747ea2
Ian Skelskey <ianskelskey@gmail.com>
2020-02-19 00:33:52 -0500

    added form design

--- Shipping_Depot/Form1.Designer.vb
+++ Shipping_Depot/Form1.Designer.vb
@@ -22,10 +22,112 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtTtlShip = New System.Windows.Forms.TextBox()
+        Me.txtFedEx = New System.Windows.Forms.TextBox()
+        Me.txtUPS = New System.Windows.Forms.TextBox()
+        Me.txtUSPS = New System.Windows.Forms.TextBox()
+        Me.txtFedExPerc = New System.Windows.Forms.TextBox()
+        Me.txtUPSPerc = New System.Windows.Forms.TextBox()
+        Me.txtUSPSPerc = New System.Windows.Forms.TextBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtTtlShip
+        '
+        Me.txtTtlShip.Location = New System.Drawing.Point(64, 130)
+        Me.txtTtlShip.Name = "txtTtlShip"
+        Me.txtTtlShip.Size = New System.Drawing.Size(100, 20)
+        Me.txtTtlShip.TabIndex = 0
+        '
+        'txtFedEx
+        '
+        Me.txtFedEx.Location = New System.Drawing.Point(64, 197)
+        Me.txtFedEx.Name = "txtFedEx"
+        Me.txtFedEx.Size = New System.Drawing.Size(100, 20)
+        Me.txtFedEx.TabIndex = 1
+        '
+        'txtUPS
+        '
+        Me.txtUPS.Location = New System.Drawing.Point(64, 253)
+        Me.txtUPS.Name = "txtUPS"
+        Me.txtUPS.Size = New System.Drawing.Size(100, 20)
+        Me.txtUPS.TabIndex = 2
+        '
+        'txtUSPS
+        '
+        Me.txtUSPS.Location = New System.Drawing.Point(64, 320)
+        Me.txtUSPS.Name = "txtUSPS"
+        Me.txtUSPS.Size = New System.Drawing.Size(100, 20)
+        Me.txtUSPS.TabIndex = 3
+        '
+        'txtFedExPerc
+        '
+        Me.txtFedExPerc.Location = New System.Drawing.Point(215, 197)
+        Me.txtFedExPerc.Name = "txtFedExPerc"
+        Me.txtFedExPerc.Size = New System.Drawing.Size(100, 20)
+        Me.txtFedExPerc.TabIndex = 4
+        '
+        'txtUPSPerc
+        '
+        Me.txtUPSPerc.Location = New System.Drawing.Point(215, 253)
+        Me.txtUPSPerc.Name = "txtUPSPerc"
+        Me.txtUPSPerc.Size = New System.Drawing.Size(100, 20)
+        Me.txtUPSPerc.TabIndex = 5
+        '
+        'txtUSPSPerc
+        '
+        Me.txtUSPSPerc.Location = New System.Drawing.Point(215, 320)
+        Me.txtUSPSPerc.Name = "txtUSPSPerc"
+        Me.txtUSPSPerc.Size = New System.Drawing.Size(100, 20)
+        Me.txtUSPSPerc.TabIndex = 6
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(405, 58)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisplay.TabIndex = 7
+        Me.btnDisplay.Text = "Button1"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(405, 103)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Button2"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.txtUSPSPerc)
+        Me.Controls.Add(Me.txtUPSPerc)
+        Me.Controls.Add(Me.txtFedExPerc)
+        Me.Controls.Add(Me.txtUSPS)
+        Me.Controls.Add(Me.txtUPS)
+        Me.Controls.Add(Me.txtFedEx)
+        Me.Controls.Add(Me.txtTtlShip)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtTtlShip As TextBox
+    Friend WithEvents txtFedEx As TextBox
+    Friend WithEvents txtUPS As TextBox
+    Friend WithEvents txtUSPS As TextBox
+    Friend WithEvents txtFedExPerc As TextBox
+    Friend WithEvents txtUPSPerc As TextBox
+    Friend WithEvents txtUSPSPerc As TextBox
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnExit As Button
 End Class
