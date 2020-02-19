ff8f16ef9c8404b0eab4c50befa6c0d1617c7ed2
Ian Skelskey <ianskelskey@gmail.com>
2020-02-19 00:44:37 -0500

    Added button text and reorganized form

--- Shipping_Depot/Form1.Designer.vb
+++ Shipping_Depot/Form1.Designer.vb
@@ -35,76 +35,76 @@ Partial Class Form1
         '
         'txtTtlShip
         '
-        Me.txtTtlShip.Location = New System.Drawing.Point(64, 130)
+        Me.txtTtlShip.Location = New System.Drawing.Point(64, 47)
         Me.txtTtlShip.Name = "txtTtlShip"
         Me.txtTtlShip.Size = New System.Drawing.Size(100, 20)
         Me.txtTtlShip.TabIndex = 0
         '
         'txtFedEx
         '
-        Me.txtFedEx.Location = New System.Drawing.Point(64, 197)
+        Me.txtFedEx.Location = New System.Drawing.Point(64, 92)
         Me.txtFedEx.Name = "txtFedEx"
         Me.txtFedEx.Size = New System.Drawing.Size(100, 20)
         Me.txtFedEx.TabIndex = 1
         '
         'txtUPS
         '
-        Me.txtUPS.Location = New System.Drawing.Point(64, 253)
+        Me.txtUPS.Location = New System.Drawing.Point(64, 135)
         Me.txtUPS.Name = "txtUPS"
         Me.txtUPS.Size = New System.Drawing.Size(100, 20)
         Me.txtUPS.TabIndex = 2
         '
         'txtUSPS
         '
-        Me.txtUSPS.Location = New System.Drawing.Point(64, 320)
+        Me.txtUSPS.Location = New System.Drawing.Point(64, 182)
         Me.txtUSPS.Name = "txtUSPS"
         Me.txtUSPS.Size = New System.Drawing.Size(100, 20)
         Me.txtUSPS.TabIndex = 3
         '
         'txtFedExPerc
         '
-        Me.txtFedExPerc.Location = New System.Drawing.Point(215, 197)
+        Me.txtFedExPerc.Location = New System.Drawing.Point(215, 92)
         Me.txtFedExPerc.Name = "txtFedExPerc"
         Me.txtFedExPerc.Size = New System.Drawing.Size(100, 20)
         Me.txtFedExPerc.TabIndex = 4
         '
         'txtUPSPerc
         '
-        Me.txtUPSPerc.Location = New System.Drawing.Point(215, 253)
+        Me.txtUPSPerc.Location = New System.Drawing.Point(215, 135)
         Me.txtUPSPerc.Name = "txtUPSPerc"
         Me.txtUPSPerc.Size = New System.Drawing.Size(100, 20)
         Me.txtUPSPerc.TabIndex = 5
         '
         'txtUSPSPerc
         '
-        Me.txtUSPSPerc.Location = New System.Drawing.Point(215, 320)
+        Me.txtUSPSPerc.Location = New System.Drawing.Point(215, 182)
         Me.txtUSPSPerc.Name = "txtUSPSPerc"
         Me.txtUSPSPerc.Size = New System.Drawing.Size(100, 20)
         Me.txtUSPSPerc.TabIndex = 6
         '
         'btnDisplay
         '
-        Me.btnDisplay.Location = New System.Drawing.Point(405, 58)
+        Me.btnDisplay.Location = New System.Drawing.Point(361, 90)
         Me.btnDisplay.Name = "btnDisplay"
         Me.btnDisplay.Size = New System.Drawing.Size(75, 23)
         Me.btnDisplay.TabIndex = 7
-        Me.btnDisplay.Text = "Button1"
+        Me.btnDisplay.Text = "Display"
         Me.btnDisplay.UseVisualStyleBackColor = True
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(405, 103)
+        Me.btnExit.Location = New System.Drawing.Point(361, 133)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(75, 23)
         Me.btnExit.TabIndex = 8
-        Me.btnExit.Text = "Button2"
+        Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(464, 245)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnDisplay)
         Me.Controls.Add(Me.txtUSPSPerc)
@@ -115,7 +115,7 @@ Partial Class Form1
         Me.Controls.Add(Me.txtFedEx)
         Me.Controls.Add(Me.txtTtlShip)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Shipping Depot"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
